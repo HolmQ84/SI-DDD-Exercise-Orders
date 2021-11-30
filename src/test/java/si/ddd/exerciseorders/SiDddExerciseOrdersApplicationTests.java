828fb1764f20812bbde943737ac5703dc5965b20
Martin Holmqvist <martin-holmqvist@hotmail.com>
2021-11-30 11:17:56 +0100

    Initial commit

---
+++ src/test/java/si/ddd/exerciseorders/SiDddExerciseOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package si.ddd.exerciseorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiDddExerciseOrdersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
